32da3e268ad1e6e198b41f1d2f584ac8be507deb
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-10-25 18:46:49 -0300

    Wire up stdout & stderr to the server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::process::Stdio;
 use anyhow::{Context, Result};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -6,9 +7,12 @@ fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+
+    let child = std::process::Command::new(command)
         .args(command_args)
-        .output()
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
         .with_context(|| {
             format!(
                 "Tried to run '{}' with arguments {:?}",
@@ -16,9 +20,13 @@ fn main() -> Result<()> {
             )
         })?;
 
+    let output = child.wait_with_output().expect("Dude, this failed big time");
+
     if output.status.success() {
         let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
+        let std_err = std::str::from_utf8(&output.stderr)?;
+        print!("{std_out}");
+        eprint!("{std_err}");
     } else {
         std::process::exit(1);
     }
